333e7d1f52bc0f89cc4758931029db800fc71f61
fcocarrascoa476 <fcocarrascoa@gmail.com>
2026-07-27 21:51:05 -0400

    publica PWA: sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "finanzas-v1";
+const SHELL = ["./index.html", "./chart.min.js", "./manifest.webmanifest",
+               "./icono-192.png", "./icono-512.png"];
+
+self.addEventListener("install", e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())));
+
+self.addEventListener("activate", e =>
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim())));
+
+self.addEventListener("fetch", e => {
+  const url = e.request.url;
+  if (url.endsWith("data.enc")) {
+    // Red primero (datos frescos); si no hay internet, cae al ultimo cacheado.
+    e.respondWith(
+      fetch(e.request).then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    // Shell: cache primero.
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
