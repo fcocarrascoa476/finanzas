ca3c5746cd639a1c3ec69c4f5fb65d2d64a28c97
fcocarrascoa476 <fcocarrascoa@gmail.com>
2026-08-04 00:58:18 -0400

    publica PWA: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "finanzas-v8";
+const CACHE = "finanzas-v9";
 const SHELL = ["./index.html", "./chart.min.js", "./manifest.webmanifest",
                "./icono-192.png", "./icono-512.png"];
 
